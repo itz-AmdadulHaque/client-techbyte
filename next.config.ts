8080f7cd2366e1e7dc948293e05bf7ad9917f80e
Amdadul Haque <amdadulhaque0041@gmail.com>
2025-10-11 22:32:52 +0600

    r2 config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["localhost"],
+    remotePatterns: [new URL("https://pub-43a158751ca142c3a51b1b0baa14b50b.r2.dev/**")],
   },
 };
 
